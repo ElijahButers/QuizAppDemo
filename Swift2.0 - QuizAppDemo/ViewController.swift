0da15cacedd7496b0266d9bfcbe4a16615771810
ElijahButers <Elijahbuters@gmail.com>
2015-08-30 23:50:24 +0300

    Added - UI Outlets.

--- Swift2.0 - QuizAppDemo/ViewController.swift
+++ Swift2.0 - QuizAppDemo/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //UI Outlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var currentNumberOfPointsLabel: UILabel!
+    @IBOutlet weak var numberOfLivesLeftLabel: UILabel!
+    @IBOutlet weak var answerOneButton: UIButton!
+    @IBOutlet weak var answerTwoButton: UIButton!
+    @IBOutlet weak var answerThreeButton: UIButton!
+    @IBOutlet weak var answerFourButton: UIButton!
+    @IBOutlet weak var nextQuestionButton: UIButton!
+    @IBOutlet weak var startOverButton: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
