0ea02eebf12a6e1f365b2dfb39507ec93b83063c
ElijahButers <Elijahbuters@gmail.com>
2015-08-31 23:54:22 +0300

    Added - 5 questions.

--- Swift2.0 - QuizAppDemo/ViewController.swift
+++ Swift2.0 - QuizAppDemo/ViewController.swift
@@ -33,11 +33,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //MARK: Questions
+        let questionOne = questionTemplate("What year did WWII start?", answerOne: "1939", answerTwo: "1940", answerThree: "1941", answerFour: "1942", correctAnswer: 1)
+        let questionTwo = questionTemplate("What day was D-Day?", answerOne: "June 6, 1944", answerTwo: "June 16, 1944", answerThree: "June 26, 1944", answerFour: "June 16, 1943", correctAnswer: 1)
+        let questionThree = questionTemplate("What country was first invaded by Germany?", answerOne: "France", answerTwo: "Belgium", answerThree: "Poland", answerFour: "Russia", correctAnswer: 3)
+        let questionFour = questionTemplate("Which article of the Weimar Constitution granted Hitler his dictator powers? ", answerOne: "Article 26", answerTwo: "Article 86", answerThree: "Article 3", answerFour: "Article 48", correctAnswer: 4)
+        let questionFive = questionTemplate("Who was the leader of the Soviet Union during World War II?", answerOne: "Lenin", answerTwo: "Trotsky", answerThree: "Stalin", answerFour: "Khruschev", correctAnswer: 3)
+
     }
     
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     //MARK: Add question
     func questionTemplate(question:String, answerOne:String, answerTwo:String, answerThree:String, answerFour:String, correctAnswer:Int) -> NSArray {
-    
+        
         //Set the question
         let quizQuestion = question
         
@@ -52,11 +67,6 @@ class ViewController: UIViewController {
         let questionAnswerArray = [question, firstAnswer, secondAnswer, thirdAnswer, fourthAnswer, rightAnswer]
         return questionAnswerArray
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
